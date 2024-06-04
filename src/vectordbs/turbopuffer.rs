af28b4f3727b43a5b34abeaeaed09b0647169fe2
Darryl Brooks <darrylbrooks13@gmail.com>
2024-06-03 23:42:02 -0500

    feat: some crud operations

--- src/vectordbs/turbopuffer.rs
+++ src/vectordbs/turbopuffer.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use indexify_internal_api::ContentMetadata;
+use indexify_internal_api::{ContentMetadata, Embedding};
 use turbopuffer_client::Client;
 
 use serde::{Deserialize, Serialize};
@@ -12,6 +12,10 @@ use crate::{
     server_config::TurboClientConfig,
     vectordbs::{FilterOperator, IndexDistance, SearchResult, VectorChunk},
 };
+
+fn hex_to_u64(hex: &str) -> Result<u64, std::num::ParseIntError> {
+    u64::from_str_radix(hex, 16)
+}
 
 #[derive(Debug)]
 pub struct TurboPuffer {
@@ -25,30 +29,94 @@ impl TurboPuffer {
         }
     }
 
-    pub fn create_client(&self) -> Result<TurboClient> {
+    pub fn create_client(&self) -> Result<Client> {
         let client_config = TurboClientConfig::new(&self.turbo_config.api_key);
-        let client = TurboClient::new(Some(client_config))
-            .map_err(|e| anyhow!("Failed to create TurboClient: {}", e))?;
+        let client = Client::new(Some(client_config));
+
         Ok(client)
     }
 
-    fn get_namespace(&self, ns: str) -> Result<NamespacedClient> {
+    pub async fn upsert(&self, namespace: String, chunks: Vec<VectorChunk>) -> Result<()> {
         let client = self.create_client()?;
-        let ns_client = client
-            .get_namespace(ns)
-            .map_err(|e| anyhow!("Failed to get namespace {}: {}", ns, e))?;
-        Ok(ns_client)
+
+        let ns = client.namespace(&namespace);
+        let mut data = None;
+
+        let ids = chunks
+            .iter()
+            .map(|c| c.content_id.clone())
+            .map(|id| hex_to_u64(&id).unwrap_or(0))
+            .collect::<Vec<String>>();
+
+        let vectors = chunks
+            .iter()
+            .map(|c| c.embedding.clone())
+            .collect::<Vec<Embedding>>();
+
+        // Will probably have to change the values to be a hashmap
+        let attributes = chunks
+            .iter()
+            .map(|c| c.metadata.clone())
+            .collect::<Vec<ContentMetadata>>();
+
+        data = serde::json!({
+            "ids": ids,
+            "vectors": vectors,
+            "attributes": attributes,
+        });
+
+        let res = ns.upsert(&data).await;
+
+        match res {
+            UpsertResponse => println!("Upserted data!"),
+            Error => println!("Failed to upsert data: {}", e),
+        };
+
+        Ok(())
     }
 
-    pub async fn list_namespaces() -> Result<Vec<String>> {
-        unimplemented!()
+    pub async fn search(
+        &self,
+        namespace: String,
+        query_embedding: Vec<f32>,
+        k: u64,
+        filters: Vec<super::Filter>,
+    ) {
+        if !filters.is_empty() {
+            // TOOD: Create filter struct
+            unimplemented!();
+        }
+
+        let client = self.create_client()?;
+
+        let ns = client.namespace(&namespace);
+
+        let query = serde::json!({
+            "query": query_embedding,
+            "k": k,
+            "filter": filter,
+        });
+
+        let res = ns.query(&query).await;
+
+        match res {
+            QueryResponse => println!("Search result: {:?}", res),
+            Error => println!("Failed to search"),
+        }
     }
 
-    pub async fn upsert(&self) -> Result<()> {
-        unimplemented!()
+    pub async fn delete_namespace(&self, namespace: String) -> Result<()> {
+        let client = self.create_client()?;
+
+        let res = client.namespace(&namespace).delete().await;
+
+        match res {
+            DeleteResponse => println!("Deleted namespace: {}", namespace),
+            Error => println!("Failed to delete namespace: {}", namespace),
+        }
     }
 
-    pub async fn delete_namespace(&self) -> Result<()> {
-        unimplemented!()
+    pub async fn update(&self) {
+        unimplemented!();
     }
 }
